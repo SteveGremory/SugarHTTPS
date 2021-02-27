13111736aea78cae08d7099c030ac73329924179
Steve Gremory <38187217+SteveGremory@users.noreply.github.com>
2021-02-27 23:52:17 +0530

    Usable condition commit

--- main.cpp
+++ main.cpp
@@ -1,26 +1,14 @@
 #include <iostream>
-#include <thread>
-#include <SugarHTTPS_RestClient.hpp>
-#include <mutex>
 
-//get(std::string url, int threadNumber, int Timeout, bool FollowRedicts, int RedictsNumber, std::string userAgent);
-
-std::mutex m;
-bool running = true;
-
-auto MakeRequest = []() {
-    for(int i = 0; i < 10; ++i) {
-        auto r = sugar::get("http://91.232.174.39/hit");
-        std::cout << r.body;
-    }
-};
-
+namespace sugar {
+    class Networking {
+    public:
+        void then() {
+            //Do After the get/post req ends
+        }
+    };
+    Networking get() {}
+}
 int main() {
-
-    std::thread threadOne(MakeRequest);
-    std::thread threadTwo(MakeRequest);
-    
-    threadOne.join();
-    threadTwo.join();
-    return 0;
+  get(12).then();
 }
